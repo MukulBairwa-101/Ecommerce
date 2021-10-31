5b0b265c447fcf1c6802f52cba6a6c05bfa09315
Mukul <mukul.bairwa@infilytics.in>
2021-10-31 15:39:31 +0530

    cart component added

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -11,7 +11,19 @@ const Cart = () => {
                     return (
                         <>
                             <div>
-                                <img src={el.image} alt="ss" />
+                                <div className="cart_image_c">
+                                    <img src={el.image} alt={el.title} />
+                                </div>
+                                <div className="text">
+                                    <h5>{el.title}</h5>
+                                    <p>{el.category}</p>
+                                    <span></span>
+                                </div>
+                                <div className="qty-btns">
+                                    <button>-</button>
+                                    <button>+</button>
+                                    <button>DEL</button>
+                                </div>
                             </div>
                         </>
 
