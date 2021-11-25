c9df6e799275bd1eaae4574371f89d5d829c5849
Mukul <mukul.bairwa@infilytics.in>
2021-11-25 19:43:54 +0530

    Chekout component added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Products from "./Component/Products/Products";
 import Item from "./Component/Products/Item";
 import {Switch ,Route } from "react-router-dom";
 import Cart from "./Component/Cart/Cart"
+import Checkout from './Component/Checkout/Checkout';
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
         <Route  exact path ="/products" component ={Products}/>
         <Route  exact path ="/products/:id" component ={Item} />
         <Route  exact path ="/cart" component ={Cart}/>
+        <Route  exact path ="/checkout" component ={Checkout}/>
         <Home />
       </Switch>
 
--- src/Component/Cart/Billing.jsx
+++ src/Component/Cart/Billing.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-
+import {Link} from "react-router-dom";
 const Billing = (props) => {
     // const [goods, setGoods] = useState(props.totalgoods);
     return (
@@ -7,6 +7,7 @@ const Billing = (props) => {
             <header>Order Summary</header>
             <h4>Total Products : {props.total[0]} </h4>
             <h4>Grand Total : $ {props.total[1]} </h4>
+            <Link to ="/checkout">Checkout</Link>
         </div>
     )
 }
